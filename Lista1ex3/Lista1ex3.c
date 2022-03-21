ed238a6b4508562bb86eee2e764edd145b0b8695
CNoski <caiobomay@gmail.com>
2022-03-21 12:50:49 -0300

    att

--- Lista1ex3/Lista1ex3.c
+++ Lista1ex3/Lista1ex3.c
@@ -5,45 +5,81 @@
     int menu;
     int ndepessoas;
     char nome[10];
-    char telefone[14];
+    char telefone[9];
     int idade[3];
+    int *ptr;
+    int i, count;
 
-    void *pBuffer = (unsigned char*)malloc(sizeof(nome)+sizeof(telefone)+sizeof(idade)+2);
+    
 
     int main(void){
         ndepessoas = 0;
 
-        do
-        {
-            menu = 0;
-            printf("Digite 1 para inserir uma pessoa a agenda.");
-            printf("Digite 2 para apagar uma pessoa a agenda.");
-            printf("Digite 3 para buscar uma pessoa na agenda.");
-            printf("Digite 4 para listar.");
-            printf("Digite 5 para sair.");
+        void *pBuffer = (unsigned char*)malloc(sizeof(nome)+sizeof(telefone)+sizeof(idade)+2);
 
-            scanf("%i", menu);
+        do{
+            menu == 0;
+            printf("Digite 1 para inserir uma pessoa a agenda. \n");
+            printf("Digite 2 para apagar uma pessoa a agenda. \n");
+            printf("Digite 3 para buscar uma pessoa na agenda. \n");
+            printf("Digite 4 para listar. \n");
+            printf("Digite 5 para sair. \n");
 
-            if (menu == 1){
-                /*inserir*/ 
-            }
-            if (menu == 2){
-                /*apagar*/
-            }
-            if (menu == 3)
-            {
-                /*buscar*/
-            }
-            if (menu == 4)
-            {
-                /*listar*/
-            }
-            if (menu < 5)
-            {
-                printf("Numero inserido é invalido.");
-            }
+            scanf("%i", &menu);
             
+            switch (menu){
+
+            case 1:
+                ndepessoas = ndepessoas + 1;
+                pBuffer = &ndepessoas;
+                printf("Digite o nome que voce deseja inserir\n");
+                scanf("%s", nome);
+                printf("Digite a idade que voce deseja inserir(maximo de 3 digitos))\n");
+                scanf("%d", idade);
+                printf("Digite o numero de telefone que voce deseja inserir(maximo de 9 digitos)\n");
+                scanf("%d", telefone);
+
+                pBuffer = realloc(pBuffer,sizeof(nome)+sizeof(telefone)+sizeof(idade)+2);
+                
+                pBuffer =  &nome;
+
+
+                printf("%i\n", *((int*)pBuffer));
+                menu = 0;               
+                break;
+            case 2:
+                ndepessoas = ndepessoas - 1;
+
+                break;
+            case 3:
+
+                printf("menu = %i \n", menu);
+                menu = 0;
+                printf("menu = %i \n", menu);
+                break;
+            case 4:
+                count = sizeof(pBuffer);
+                printf("=========================AGENDA============================ \n");
+                for ( i = sizeof(pBuffer); i < count; i++)
+                {
+                
+                }
+                
+
+                printf("======================FIM DA AGENDA============================\n", menu);
+                menu = 0;
+                break;
+            case 5:
             
-        } while (menu != 5);
-    return(0);
-    }
+                printf("menu = %i \n", menu);
+                free(pBuffer);
+                break;
+            }
+         
+        } while (menu < 5);
+        
+        if(menu > 5){
+            printf("NUMERO INVALIDO");
+        }
+        return(0);
+    } 
