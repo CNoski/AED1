9b865e04d7acc0aa6af4ef33932f77a1cb598e4d
CNoski <caiobomay@gmail.com>
2022-04-23 16:58:27 -0300

    Agenda

--- Codigo Agenda/CodigoAgenda.c
+++ Codigo Agenda/CodigoAgenda.c
@@ -1,102 +1,86 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define NODO_TAM sizeof(void*)*2+sizeof(int)*2+sizeof(char)*30
 
 
 int main(){
 
-    void *Ancora ;
     
-    void *pBuffer;
-    char *Nometemp[30];
-    int *Telefonetemp;
-    int *Idadetemp;
+    
+    void *pBuffer = (void*)malloc(sizeof(char)*100);
+    if(pBuffer == NULL){
+    printf("Erro! Memoria nao alocada.");
+    return EXIT_FAILURE;
+    }
 
-    int *loop_menu = 1;
-    int *valor_menu = 0;
+
+    /*
+    pbuffer[0] é um inteiro que define o while da linha 32
+    pbuffer[3] é um inteiro que defino o valor do menu
+    pbuffer[7] = Ancora (começo da lista) 46 bytes
+
+
+    */
+
+    void *Ancora = NULL ;
+
+    void **proximo;
+    void *nodo;
+    void **nodoleitor;
+
     
-    while (loop_menu){
+    while (((int*)pBuffer)[0]){
 
         printf("Digite 1 para adicionar um nome \n");
         printf("Digite 2 para remover um nome \n");
         printf("Digite 3 para buscar o programa \n");
         printf("Digite 4 para listar os nomes \n");
         printf("Digite 5 fechar o programa \n");
-        scanf("%d", valor_menu);
+        scanf("%i", &((int*)pBuffer)[4]);
 
+        switch(((int*)pBuffer)[4]){
 
-        if (*valor_menu = 1){
+            case 1:
             
-            criar(*Nometemp, *Telefonetemp, *Idadetemp);
-            push(&Ancora, criar);
+                nodoleitor = &((void*)pBuffer)[7];
+                proximo = *nodoleitor + sizeof(char)*42;
+                while ((*nodoleitor) && (strncmp(*nodoleitor + sizeof(void*),*proximo + sizeof(void*),sizeof(char)*30)) < 1){
+                    
+                
+                }
+                nodo = (void*)malloc(sizeof(NODO_TAM));
 
-        
-        }
-        else if (*valor_menu = 2){
 
+            break;
 
-        }
-        else if (*valor_menu = 3){
-            
+            case 2:
 
-        }
-        else if (*valor_menu = 4){
-            
+                printf("\n2");
 
-        }
-        else if (*valor_menu = 5){
-            
+            break;
+
+            case 3:
+
+            printf("\n3");
+
+            break;
+
+            case 4:
+
+            printf("\n4");
+
+            break;
+
+            case 5:
+
+            printf("Saindo do Aplicativo.");
+            free(pBuffer);
+            return EXIT_SUCCESS;
+
+            break;           
+
 
         }
     }
-    
-    
-    
-    return 0;
 }
-
-
-
-void criar(char *Nometemp[30] , int *Telefonetemp, int *Idadetemp){
-
-    void *nodo = (void *)malloc( sizeof(char) * 30 + sizeof(int) * 2 + sizeof(void*) );/* Aloca um espaço para 4 int e 30 char*/
-    void *comeconodo;//sizeof(int)
-    comeconodo = &nodo;/* cria um ponteiro pro começo do nodo */
-
-    nodo = nodo + sizeof(void*);/*pula o espaço pro nodo anterior*/
-
-    printf("Insira o Nome do contato.\n");
-    scanf("%s", &Nometemp);/* recebe o nome*/
-    memcpy(nodo, Nometemp, 30);/* copia o telefone pra dentro do nodo*/
-
-    nodo = nodo + sizeof(char) * 30;/* pula o espaço pro que está a informação do nome */
-
-    printf("Insira o Numero do contato.\n");
-    scanf("%d",&Telefonetemp);/* recebe o telefone*/
-    memcpy(nodo, Telefonetemp, sizeof(int));/* copia o telefone pra dentro do nodo*/
-
-    nodo = nodo + sizeof(int);/* pula o espaço que armazena o telefone*/
-
-    printf("Insira a Idade do contato.");
-    scanf("%d",&Idadetemp);/* recebe a idade*/
-    memcpy(nodo, Idadetemp, sizeof(int));/* Copia a idade pra dentro do nodo*/
-
-    nodo = comeconodo; 
-
-    return nodo;
-
-}
-
-void push(void **start, void **proximo){
-
-    void **indice = start;
-    void **indice_proximo = proximo;
-
-    while ((*indice) && strncmp((*indice) + 4 , (*proximo) + 4, 30)< 1)
-    {
-        /* code */
-    }
-    
-
-}
-
